09a86a72c0f24813fbcbd147852499281bfbdebd
Hernan Pinos <hernan.pinos@outlook.com>
2020-04-07 15:12:32 -0500

    Creacion de la aplicacion web mvc para el proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Proyecto_ASP_NET_CORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
